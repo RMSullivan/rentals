8abd5bc91f84734ce7cf5f2dd98bb79373a7db6a
github username <github email address>
2018-08-03 12:33:40 -0400

    extend interface
    Expand CLI interface to say hello, get info and say goodbye

--- lib/rentals/cli.rb
+++ lib/rentals/cli.rb
@@ -1,55 +1,51 @@
+#CLI controller
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
 
-class Rentals::CLI
+class Rentals::CLI #class called CLI that is instantiated using the call method
 
-  def call
+  def call #call method
+    zip_code
+    prompt
+    goodbye
+
+  end
+
+  def zip_code
     puts "************* Find Rentals In Any US Zip Code. *************"
     puts ""
     puts "Please enter a five digit zip code to search available rentals on Realtor.com:"
     zip_code = gets.strip
-
     url = "https://www.realtor.com/apartments/#{zip_code}"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    total = parsed_page.css('span#search-result-count.page-title').text.tr("\n","").to_i
-
     puts ""
     puts "Searching https://www.realtor.com/apartments/#{zip_code} for rentals...."
     puts ""
-    puts "There are #{total} listings in #{zip_code}"
+    puts puts "Compiling your listings for #{zip_code}....."
     puts ""
+    puts "Here are the available listings for #{zip_code}"
+    end
 
-
-    listings = Array.new
-    rental_listings = parsed_page.css('li.component_property-card.js-component_property-card')
-    page = 1
-    per_page = rental_listings.count #48
-    total = parsed_page.css('span#search-result-count.page-title').text.tr("\n","").to_i
-    last_page = 1 + (total / per_page).round #2
-    while page <= last_page
-       pagination_url = "https://www.realtor.com/apartments/#{zip_code}/pg-#{page}"
-       pagination_unparsed_page = HTTParty.get(pagination_url)
-       pagination_parsed_page = Nokogiri::HTML(pagination_unparsed_page)
-       pagination_rental_listings = pagination_parsed_page.css('li.component_property-card.js-component_property-card')
-       pagination_rental_listings.each do |rental_listing|
-        listing = {
-          address: rental_listing.css('span.listing-street-address').text,
-          city: rental_listing.css('span.listing-city').text,
-          number_of_bedrooms: rental_listing.css('span.data-value.meta-beds-display').text,
-          price: rental_listing.css('span.data-price').text,
-        }
-        puts listing
-        puts ""
+  def prompt
+    input = nil
+    while input != "exit"
+      puts ""
+      puts "Enter the listing number that you would like more information on, type new to start a new search, or type exit to quit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More details about address goes here"
+      when "new"
+        zip_code
+      else
+        puts "Invalid Entry, Please Try Again!"
+        puts "Enter the listing number for more information, type new to start over, or exit to quit"
 
       end
-      page += 1
     end
-    puts "******************** end of search *********************"
-    puts ""
-
   end
-
+  def goodbye
+    puts "Check back soon for more listings!"
+  end
 end
